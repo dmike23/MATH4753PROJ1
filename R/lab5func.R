5b3e458347f57a8b25a14a62e60f6422679a9883
Michael Hogsett <michael.hogsett-1@ou.edu>
2020-11-17 13:39:32 -0600

    Update lab5func.R

--- R/lab5func.R
+++ R/lab5func.R
@@ -1,28 +1,16 @@
 
-#P(Y=10), Y~NegBin(p=0.4,r=3).   (Book theory)
-#  This means that in R we need n=3 (size), x=y-r=10-3=7
- # Nu Failures, Nu of successes, prob success
-#' fucntion for Lab 5
-#'
+#' Negative Binomial Probability Distribution Function
+#' This returns the probability distribution for a negative binomial random variable Y
 #' @param y
 #' @param r
 #' @param p
 #'
-#' @return
+#' @return Probability distribution 
 #' @export
 #'
 #' @examples
+# mynbin(10,3,0.4)
+
 mynbin=function(y,r,p){
   choose(y-1,r-1)*p^r*(1-p)^(y-r)
 }
-
-mynbin(10,3,0.4)
-
-    #P(Y=10), Y~NegBin(p=0.4,r=3).   (Book theory)
-#  This means that in R we need n=3 (size), x=y-r=10-3=7
-dnbinom(7,3,0.4)  # Nu Failures, Nu of successes, prob success
-mynbin=function(y,r,p){
-choose(y-1,r-1)*p^r*(1-p)^(y-r)
-}
-mynbin(10,3,0.4)
-
